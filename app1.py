02d8c520fc4600d8cfc4335af42c241aa8f2d1cd
PuritySithole <66123369+PuritySithole@users.noreply.github.com>
2020-06-10 06:27:41 +0200

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,34 +1,36 @@
 from flask import Flask,render_template,url_for,request,flash
 #import pandas as pd
-from sklearn.externals import joblib
+import joblib
 import numpy as np
 import pickle
 import os
 
 
+
 app = Flask(__name__, template_folder='templates')
+app.secret_key = c'_100#y2L"remembrancez\n\xec]/'
 #model = pickle.load(open('model_naive.pkl', 'rb'))
 @app.route('/')
 def home():
-	return render_template('app.html')
+    return render_template('index.html')
 
-def getParameters():
-    parameters = []
+def getUserInput():
+    user_input = []
     #parameters.append(request.form('name'))
-    parameters.append(request.form['Age'])
-    parameters.append(request.form['Serum Cholesterol'])
-    parameters.append(request.form['Trestbps'])
-    parameters.append(request.form['Thalach'])
-    parameters.append(request.form['Old Peak'])
+    user_input.append(request.form['Age'])
+    user_input.append(request.form['Serum Cholesterol'])
+    user_input.append(request.form['Trestbps'])
+    user_input.append(request.form['Thalach'])
+    user_input.append(request.form['Old Peak'])
     
     
-    return parameters
+    return user_input
 
 @app.route('/predict',methods=['POST'])   
 def predict():
     if request.method=='POST':
-        parameters= getParameters()
-        x = np.array(parameters).reshape(1, -1)
+        user_input= getUserInput()
+        x = np.array(user_input).reshape(1, -1)
      
 
 
@@ -42,11 +44,18 @@ def predict():
         '''model_path = os.path.join(os.path.dirname(__file__), 'model_naive.sav')
         clf = joblib.load(model_path)'''
 
-        y = clfr.predict(inputFeature.reshape(-1,1))
-    return render_template('app.html',prediction = y[1])
+        prediction = clfr.predict(inputFeature.reshape(-1,1))
+        predict = np.sort(prediction)[::-1]
+        if predict[0]==1:
+            flash("You have been diagnosed of a heart disease","warning")
+        else:
+           flash("You haven't been diagnosed of a heart disease","success")
+   
+              
+          
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    #model = pickle.load(open('C:/Users/Ree/Desktop/prototyping/model_naive.pkl', 'rb'))
     clfr=joblib.load('model_naive.sav')
     app.run(debug=True)
 
@@ -54,33 +63,3 @@ if __name__ == '__main__':
 
 
 
-'''
-@app.route('/predict',methods=['POST'])
-def predict():
-    model = pickle.load(open('model_naive_test.pkl', 'rb'))
-    #clfr = joblib.load(model)
-
-@app.route('/print',methods=['POST','GET'])
-def displayParameters():
-    if request.method=='POST':
-        parameters= getParameters()
-        inputFeature = np.asarray(parameters).reshape(-1,-1)
-        my_prediction = clfr.predict(inputFeature)
-    return render_template('app.html',my_prediction=my_prediction)  
-
-    if request.method == 'POST':
-        parameters = getParameters()
-        inputFeature = np.asarray(parameters).reshape(1,-1)
-        my_prediction = model.predict(inputFeature)
-    return render_template('index.html',prediction = int(my_prediction[0]))
-
-     scaler_path = os.path.join(os.path.dirname(__file__), 'model_naive.pkl')
-    scaler = None
-    with open(scaler_path, 'rb') as f:
-        scaler = pickle.load(f)
-
-    #x = scaler.transform(x)
-
-    model_path = os.path.join(os.path.dirname(__file__), 'model_naive.sav')
-    clf = joblib.load(model_path)
-'''
